2b21b6fcddb04d7fa076175b29d5df5b8bfdd2ee
Kiarash Vosough <kiarash.vosough@divar.ir>
2022-05-03 18:09:29 +0430

    first commit

---
+++ WefoxPokedex/WefoxPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  WefoxPokedexApp.swift
+//  WefoxPokedex
+//
+//  Created by Kiarash Vosough on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WefoxPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
