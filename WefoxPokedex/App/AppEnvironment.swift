0beeaba5468bad0573a56d3eb49793aff822a2f3
Kiarash Vosough <kiarash.vosough@divar.ir>
2022-05-04 20:42:54 +0430

    implemented other part for some testing

--- WefoxPokedex/App/AppEnvironment.swift
+++ WefoxPokedex/App/AppEnvironment.swift
@@ -17,23 +17,64 @@ struct AppEnvironment {
 }
 
 extension AppEnvironment {
+    
     static func bootstrap() -> AppEnvironment {
         let appState = Store<AppState>(AppState())
         
-        // configuring Services
-        let services = configuredServices()
+        // Configuring Persistent Repositories
+        let persistentRepositories = configuredPersistentRepositories(appState: appState)
         
-        // configuring DIContainer
+        // Configuring network repositories
+        let networkRepositories = configuredNetworkRepositories(session: configuredURLSession())
+        
+        // Configuring Services
+        let services = configuredServices(pokemonSearchRepository: networkRepositories.pokemonSearchRepository,
+                                          pokemonSearchPersistentRepository: persistentRepositories.pokemonSearchPersistentRepository,
+                                          appState: appState)
+        
+        // Configuring DIContainer
         let diContainer = DIContainer(appState: appState, services: services)
         
-        // configuring AppEventsHandler
+        // Configuring AppEventsHandler
         let systemEventsHandler = AppEventsHandlerImpl(container: diContainer)
 
         return AppEnvironment(container: diContainer,
                               systemEventsHandler: systemEventsHandler)
     }
     
-    private static func configuredServices() -> DIContainer.Services {
-        return DIContainer.Services()
+    private static func configuredURLSession() -> URLSession {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 10
+        configuration.timeoutIntervalForResource = 60
+        configuration.waitsForConnectivity = false
+        configuration.httpMaximumConnectionsPerHost = 5
+        configuration.requestCachePolicy = .returnCacheDataElseLoad
+        configuration.urlCache = .shared
+        return URLSession(configuration: configuration)
+    }
+    
+    private static func configuredNetworkRepositories(session: URLSession) -> DIContainer.WebRepositories {
+        
+        let pokemonSearchRepository = PokemonSearchRepositoryImpl(session: session,
+                                                                  baseURL: "https://pokeapi.co/api")
+        
+        return DIContainer.WebRepositories(pokemonSearchRepository: pokemonSearchRepository)
+    }
+    
+    private static func configuredServices(pokemonSearchRepository: PokemonSearchRepository,
+                                           pokemonSearchPersistentRepository: PokemonSearchPersistentRepository,
+                                           appState: Store<AppState>) -> DIContainer.Services {
+        
+        let pokemonSerachServices = PokemonSerachServicesImpl(pokemonSearchRepository: pokemonSearchRepository,
+                                                              pokemonSearchPersistentRepository: pokemonSearchPersistentRepository,
+                                                              appState: appState)
+        
+        return DIContainer.Services(pokemonSerachServices: pokemonSerachServices)
+    }
+    
+    private static func configuredPersistentRepositories(appState: Store<AppState>) -> DIContainer.DBRepositories {
+        let persistentStore = CoreDataStack(version: CoreDataStack.Version.actual)
+        let pokemonSearchPersistentRepository = PokemonSearchPersistentRepositoryImpl(persistentStore: persistentStore)
+        return DIContainer.DBRepositories(pokemonSearchPersistentRepository: pokemonSearchPersistentRepository)
     }
 }
--- WefoxPokedex/App/Scenes/MainAppSceneDelegate.swift
+++ WefoxPokedex/App/Scenes/MainAppSceneDelegate.swift
@@ -15,7 +15,12 @@ final class MainAppSceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let contentView = ContentView()
+        
+        guard let appEnvironment = self.appEnviroment else {
+            return
+        }
+        
+        let contentView = ContentView(viewModel: ContentView.ViewModel(container: appEnvironment.container))
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: contentView)
