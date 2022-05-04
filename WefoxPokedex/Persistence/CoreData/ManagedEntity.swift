a9dca0ba816936db2d90d192c5e7977e20e8b59a
Kiarash Vosough <kiarash.vosough@divar.ir>
2022-05-04 14:30:24 +0430

    ManagedEntity implemented

---
+++ WefoxPokedex/Persistence/CoreData/ManagedEntity.swift
@@ -0,0 +1,28 @@
+//
+//  ManagedEntity.swift
+//  WefoxPokedex
+//
+//  Created by Kiarash Vosough on 5/4/22.
+//
+
+import CoreData
+
+// MARK: - ManagedEntity
+
+protocol ManagedEntity: NSFetchRequestResult { }
+
+extension ManagedEntity where Self: NSManagedObject {
+    
+    static var entityName: String {
+        return String(describing: Self.self)
+    }
+    
+    static func insertNew(in context: NSManagedObjectContext) -> Self? {
+        return NSEntityDescription
+            .insertNewObject(forEntityName: entityName, into: context) as? Self
+    }
+    
+    static func newFetchRequest() -> NSFetchRequest<Self> {
+        return .init(entityName: entityName)
+    }
+}
