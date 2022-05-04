5db98f3411771c7f92271ae2d5596a598baac96b
Kiarash Vosough <kiarash.vosough@divar.ir>
2022-05-04 11:44:14 +0430

    NetworkAPIVersion implemented to handle api versioning

---
+++ WefoxPokedex/Networking/NetworkAPIVersion.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkAPIVersion.swift
+//  WefoxPokedex
+//
+//  Created by Kiarash Vosough on 5/4/22.
+//
+
+import Foundation
+
+enum NetworkAPIVersion: String {
+    case v2 = "/v2"
+    case noVersion = ""
+}
