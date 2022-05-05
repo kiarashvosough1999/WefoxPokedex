e596b350a32da670c9b502d63a3c5d9133281345
Kiarash Vosough <kiarash.vosough@divar.ir>
2022-05-05 10:51:59 +0430

    provided better converting and equality checking in PokemonSearchPersistentRepositoryImpl

--- WefoxPokedex/Repositories/PokemonSearchPersistentRepository.swift
+++ WefoxPokedex/Repositories/PokemonSearchPersistentRepository.swift
@@ -35,29 +35,40 @@ struct PokemonSearchPersistentRepositoryImpl: PokemonSearchPersistentRepository 
     }
     
     func savePokemon(model: PokemonSearchModel, imageData: Data) -> AnyPublisher<Bool, Error> {
-        persistentStore.insert(Pokemon.self) { [imageData] object in
-            object.imageData = imageData
-            object.apiId = model.id
-            object.baseExperience = model.baseExperience
-            object.height = model.height
-            object.name = model.name
-            object.order = model.order
-            object.frontDefault = model.frontDefault
-            object.types = model.types.joined(separator: ",")
-            object.weight = model.weight
+        persistentStore.insert(Pokemon.self) { [model, imageData] object in
+            object.from(from: model, imageData: imageData)
         }
-        .map { createdObject in
-            createdObject.apiId == model.id &&
-            createdObject.baseExperience == model.baseExperience &&
-            createdObject.height == model.height &&
-            createdObject.name == model.name &&
-            createdObject.order == model.order &&
-            createdObject.frontDefault == model.frontDefault &&
-            createdObject.types == model.types.joined(separator: ",") &&
-            createdObject.weight == model.weight
+        .map { [model, imageData] createdObject in
+            createdObject.match(with: model, imageData: imageData)
         }
         .mapError(mapPersistentDoimanErrors(error:))
         .mapError { $0 as Error }
         .eraseToAnyPublisher()
+    }
+}
+
+fileprivate extension Pokemon {
+    func from(from apiModel: PokemonSearchModel, imageData: Data) {
+        self.imageData = imageData
+        self.apiId = apiModel.id
+        self.baseExperience = apiModel.baseExperience
+        self.height = apiModel.height
+        self.name = apiModel.name
+        self.order = apiModel.order
+        self.frontDefault = apiModel.frontDefault
+        self.types = apiModel.types.joined(separator: ",")
+        self.weight = apiModel.weight
+    }
+    
+    func match(with apiModel: PokemonSearchModel, imageData: Data) -> Bool {
+        self.apiId == apiModel.id &&
+        self.baseExperience == apiModel.baseExperience &&
+        self.height == apiModel.height &&
+        self.name == apiModel.name &&
+        self.order == apiModel.order &&
+        self.frontDefault == apiModel.frontDefault &&
+        self.types == apiModel.types.joined(separator: ",") &&
+        self.weight == apiModel.weight &&
+        self.imageData == imageData
     }
 }
